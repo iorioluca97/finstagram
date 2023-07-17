9a66c0eae79ccf73c26cbf180fee33f15be0c6df
Luca <luca.acer@gmail.com>
2023-07-17 11:28:51 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'pages/HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: HomePage()),
+    );
+  }
+}
